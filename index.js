f6c53e6264b1e69390d96f4f523dab57a5c32df1
Prajwal Pawar <prajpawar23@gmail.com>
2022-11-08 03:04:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var rn1=Math.ceil(6*Math.random());
-var imgsrc="images/dice"+rn1+".png";
+var imgsrc="dice"+rn1+".png";
 document.querySelectorAll("img")[0].setAttribute("src", imgsrc);
 
 var rn2=Math.ceil(6*Math.random());
-var imgsrc="images/dice"+rn2+".png";
+var imgsrc="dice"+rn2+".png";
 document.querySelectorAll("img")[1].setAttribute("src", imgsrc);
 
 if(rn1>rn2)
